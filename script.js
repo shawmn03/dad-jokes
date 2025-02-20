b2d3896dcacbefde9dcf4f63fceccececd714f77
Megan Shaw <shawmn@vcu.edu>
2025-02-20 12:45:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-function generateJoke() {
-   const config = {
-     headers: {
-       Accept: 'application/json',
-     },
-   };
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
 
-   fetch('https://icanhazdadjoke.com', config)
-     .then((res) => res.json())
-     .then((data) => {
-       jokeEl.textContent = data.joke;
-     });
- }
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}
 
 generateJoke();
 
