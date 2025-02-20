5e2f7b21b77a23405290ea38e2264a6c9c62bc0b
Megan Shaw <shawmn@vcu.edu>
2025-02-20 12:28:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+function generateJoke() {
+   const config = {
+     headers: {
+       Accept: 'application/json',
+     },
+   };
+
+   fetch('https://icanhazdadjoke.com', config)
+     .then((res) => res.json())
+     .then((data) => {
+       console.log(data);
+     });
+ }
+
+generateJoke();
